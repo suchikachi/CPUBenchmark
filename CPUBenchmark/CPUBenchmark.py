852a28a93a0f38818b1ac9018a2d31b8068db6be
Richard Hovsepyan <RH149532@hrsfc.ac.uk>
2023-05-23 14:21:36 +0100

    oop

--- CPUBenchmark/CPUBenchmark.py
+++ CPUBenchmark/CPUBenchmark.py
@@ -1,16 +1,33 @@
+import cpuinfo
+import time
+
 def __main__():
     print("pymark test")
-    chooseVersion()
+    chooseVer(selection())
 
-def chooseVersion():
+def selection():
     while (True):
         choice = int(input("1. Quick test (usually less accurate)\n2. Normal test (accurate)"))
         try:
             if choice == 1:
-                pass
-                #shortTest()
+                return 1
             if choice == 2:
-                pass
-                #longTest()
+                return 2
         except:
             print("Error: Please choose a valid option")
+
+def chooseVer(selection):
+    if selection == 1:
+        shortTest()
+    if selection == 2:
+        longTest()
+
+def shortTest():
+    print("Beginning short test!")
+    for x in 15:
+        print(x)
+
+def longTest():
+    print("Beginning long test!")
+    for x in 50:
+        print(x)
