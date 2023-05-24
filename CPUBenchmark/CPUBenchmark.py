b140fa15ec2074a2a3fb6cbe4350adb8f689b138
Richard Hovsepyan <RH149532@hrsfc.ac.uk>
2023-05-24 11:58:07 +0100

    first implementation of progress bar

--- CPUBenchmark/CPUBenchmark.py
+++ CPUBenchmark/CPUBenchmark.py
@@ -1,33 +1,56 @@
 import cpuinfo
+import easygui
+from Color_Console import *
 import time
+from progressbar import ProgressBar
 
-def __main__():
-    print("pymark test")
-    chooseVer(selection())
 
-def selection():
-    while (True):
-        choice = int(input("1. Quick test (usually less accurate)\n2. Normal test (accurate)"))
+
+def main():
+    li = [ 'black','bright white' ]
+    
+    print("PyMark test")
+    color ( text = "red" )
+    playSim(chooseVer())
+
+def chooseVer():
+    color ( text = "white" )
+    while True:
         try:
-            if choice == 1:
-                return 1
-            if choice == 2:
-                return 2
+            choice = int(input("1. Quick test (usually less accurate)\n2. Normal test (accurate)\n"))
+            if choice == 1 or choice == 2:
+                return choice
+            else:
+                easygui.msgbox("Error: Please enter a valid option!", title="you silly user!!")
         except:
-            print("Error: Please choose a valid option")
+            easygui.msgbox("Error: Please enter a valid option!", title="you silly user!!")
 
-def chooseVer(selection):
-    if selection == 1:
+def playSim(type):
+    if type == 1:
         shortTest()
-    if selection == 2:
+    elif type == 2:
         longTest()
 
 def shortTest():
     print("Beginning short test!")
-    for x in 15:
-        print(x)
+    # todo short test
+    main()
+        
 
 def longTest():
     print("Beginning long test!")
-    for x in 50:
-        print(x)
+    # todo long test
+    main()
+
+
+pbar = ProgressBar()
+
+def job():
+    for i in pbar(range(5)):
+        print(i)
+
+job()
+
+
+if __name__ == "__main__":
+    main()
